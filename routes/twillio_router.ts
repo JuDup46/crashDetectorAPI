b1115c6cc17145aa294131de7aaf862144ea79f3
Fred <frederic.Flaceliere@gmail.com>
2022-03-11 09:18:45 +0100

    update twilio sms

--- routes/twillio_router.ts
+++ routes/twillio_router.ts
@@ -8,14 +8,14 @@ import {DatabaseUtils} from "../database/database";
 
 const twillio_router = express.Router();
 
-twillio_router.get("/:email/:coordonne",async function (req, res) {
+twillio_router.get("/:email/:coordonnee",async function (req, res) {
     const connection = await DatabaseUtils.getConnection();
     const userController = new UserController(connection);
     const userData = await userController.getUserTelWithEmail(req.params.email)
     client.calls
         .create({
             twiml: '<Response>\n' +
-                `<Say voice="alice" language="fr-fr">Merci ${userData!.firstName} ${userData!.lastName}, votre demande a été traité !</Say>\n` +
+                `<Say voice="alice" language="fr-fr">Une de vos connaissances : ${userData!.firstName} ${userData!.lastName}, a fait une chute en scooter</Say>\n` +
                 '<Play>http://demo.twilio.com/docs/classic.mp3</Play>\n' +
                 '</Response>',
             to:  userData!.tel,
@@ -25,7 +25,8 @@ twillio_router.get("/:email/:coordonne",async function (req, res) {
         .catch((reason: any) => console.log(reason));
 
     client.messages.create({
-        body:`Merci ${userData!.firstName} ${userData!.lastName}, votre demande a été traité ! La position de la victime est à cette adresse : ${req.params.coordonne}`,
+        body:`Une de vos connaissances : ${userData!.firstName} ${userData!.lastName}, a fait une chute en scooter ! La position de la personnne est à cette adresse : 
+https://maps.google.com/?q=${req.params.coordonnee}\nCordialement, Brum rider`, //51.03841,-114.01679
         from:"+12253073611",
         to:userData!.tel
     }).then((call: { sid: any; }) => console.log(call.sid))
